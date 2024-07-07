0a063af733265c21542f17257c3f067405487cf6
Ojima <72498960+akbenngold@users.noreply.github.com>
2024-07-07 21:01:55 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ function Hero() {
         backgroundPosition: "top",
       }}
     >
-      <div className="hero-content flex flex-col max-w-full text-neutral-content">
+      <div className="hero-content flex flex-col items-start justify-center max-w-full text-neutral-content">
         
           <h1 className="mb-5 text-xl md:text-5xl font-semibold lg:text-6xl">
             Discover Your Signature <br/> Scent With TimbuShop
@@ -18,7 +18,7 @@ function Hero() {
           <p className="mb-5 text-white text-xs md:text-base lg:text-xl">
             Get 15% off any Order above N4500
           </p>
-          <button className="btn btn-primary px-6 py-4 md:px-6 text-white font-normal bg-black border-none w-20 md:w-36 hover:bg-[#D9455F]">
+          <button className="btn btn-primary px-6 py-4 md:px-6 text-white font-normal bg-black border-none w-24 md:w-36 hover:bg-[#D9455F]">
             <a href="#products">Shop now</a>
           </button>
         
