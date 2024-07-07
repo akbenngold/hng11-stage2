e86e034c2943a0c1ff5b9b746174b49c59235ea1
akbenngold <akbenngold@gmail.com>
2024-07-07 17:02:50 +0100

    corrections

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -74,7 +74,7 @@ function Cart() {
       )}
       <div className="flex-col hidden md:flex gap-6 items-center justify-center">
         <h2 className="text-xl">Customers who viewed this also viewed</h2>
-        <div className="grid gap-4 grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {" "}
           {viewed.map((product) => (
             <Card
