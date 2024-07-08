6bd5b639792d43f9eff13affff28dd0807bf9aee
Ojima <72498960+akbenngold@users.noreply.github.com>
2024-07-08 11:17:59 +0100

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -18,7 +18,7 @@ function Cart() {
       </h1>
       {/*  */}
       {/*  */}
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 w-full place-items-center">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 w-full place-items-center">
         {products.map((product) => {
           if (cartItems[product.id] !== 0) {
             return (
