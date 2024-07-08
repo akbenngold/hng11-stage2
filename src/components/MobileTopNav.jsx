2935e77cb9d647831d438c3360174f1ba47dc9fb
akbenngold <akbenngold@gmail.com>
2024-07-08 12:16:04 +0100

    done

--- src/components/MobileTopNav.jsx
+++ src/components/MobileTopNav.jsx
@@ -11,21 +11,23 @@ function MobileTopNav() {
       className={`navbar w-full absolute top-0 lg:hidden transition-all duration-500 z-50`}
     >
       <div className="flex-1 gap-2">
-        <svg
-          width="32"
-          height="32"
-          viewBox="0 0 32 32"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M6.66699 22.6666H25.3337M6.66699 16H25.3337M6.66699 9.33331H25.3337"
-            stroke="black"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
+        <NavLink to="/">
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6.66699 22.6666H25.3337M6.66699 16H25.3337M6.66699 9.33331H25.3337"
+              stroke="black"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </NavLink>
 
         <a className="mx-[-1rem] btn btn-ghost text-xl fredoka flex items-center">
           <img className="h-6" src={logo} alt="logo" /> TimbuCloudShop
